afb91b340ad946c017558e3c43aa0f58c78c7006
Jon-myers <jon.myers.sound@gmail.com>
2021-12-08 16:25:48 -0800

    generating from meta_params

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 from piece import build
+import sys
 # from line_profiler import LineProfiler
 # lprofiler = LineProfiler()
 # lp_wrapper = lprofiler(build)
 # @profile
-dur_tot = 24 * 60
+
+
+
 f = 0.6
-num_of_modes = 16
+dur_tot = 24 * 60
 noc = 14
+num_of_modes = 16
+
+f = float(sys.argv[1])
+dur_tot = float(sys.argv[2])
+noc = int(float(sys.argv[3]))
+num_of_modes = int(float(sys.argv[4]))
+print(f, dur_tot, noc, num_of_modes)
 
 build(use_pickles=False, dur_tot=dur_tot, f=f, num_of_modes=num_of_modes, noc=noc)
 # lp_wrapper(save_pickle=True)
--- terminal_call.py
+++ terminal_call.py
@@ -1,5 +1,12 @@
 import os
 import sys
+import json
 
-os.system("python main.py")
-os.system("sclang sc/nrt_all.scd")
+mp = json.load(open('JSON/meta_params.JSON'))
+for i in range(1):
+
+    f, dur, cycles, chords = mp[i]
+
+
+    os.system("python main.py " + str(f) + ' ' + str(dur) + ' ' + str(cycles) + ' ' + str(chords))
+    os.system("sclang sc/nrt_all.scd " + str(dur + 30) + ' ' + str(i))
