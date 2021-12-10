fb7253b0e5fdce3bba92b67235429650cc46e347
Jon-myers <jon.myers.sound@gmail.com>
2021-12-09 22:10:46 -0800

    failsafe where audio must exist to move on.

--- terminal_call.py
+++ terminal_call.py
@@ -1,12 +1,18 @@
 import os
 import sys
 import json
+from os.path import exists
 
 mp = json.load(open('JSON/meta_params.JSON'))
-for i in range(5):
+
+i = 0
+while i < 10:
 
     f, dur, cycles, chords = mp[i]
 
 
     os.system("python3 main.py " + str(f) + ' ' + str(dur) + ' ' + str(cycles) + ' ' + str(chords))
     os.system("sclang sc/nrt_all.scd " + str(dur + 30) + ' ' + str(i))
+    path = '../audio/' + str(i) + '.wav'
+    if exists(path):
+        i += 1
