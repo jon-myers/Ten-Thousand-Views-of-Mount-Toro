f341d16534c05cca22e8b31d0eda1b743055f76c
Jon Myers <jon.myers.sound@gmail.com>
2021-12-10 03:24:25 +0000

    running on server

--- terminal_call.py
+++ terminal_call.py
@@ -3,10 +3,10 @@ import sys
 import json
 
 mp = json.load(open('JSON/meta_params.JSON'))
-for i in range(1):
+for i in range(5):
 
     f, dur, cycles, chords = mp[i]
 
 
-    os.system("python main.py " + str(f) + ' ' + str(dur) + ' ' + str(cycles) + ' ' + str(chords))
+    os.system("python3 main.py " + str(f) + ' ' + str(dur) + ' ' + str(cycles) + ' ' + str(chords))
     os.system("sclang sc/nrt_all.scd " + str(dur + 30) + ' ' + str(i))
