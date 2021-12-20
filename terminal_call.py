f1bb401278790a5820a0aa13708d7b670e64586e
Jon-myers <jon.myers.sound@gmail.com>
2021-12-20 15:03:27 -0800

    got rid of mp3 bullshit for now

--- terminal_call.py
+++ terminal_call.py
@@ -1,6 +1,6 @@
 import os, re, sys, json, glob
 from os.path import exists
-from pydub import AudioSegment
+# from pydub import AudioSegment
 
 mp = json.load(open('JSON/meta_params.JSON'))
 
@@ -16,27 +16,23 @@ else:
 
 af_max = af_nums[-1]
 
-
-if pre_set_af_nums.count(pre_set_af_nums[-1]) == 2:
-    i = af_max + 1
-else:
-    i = af_max
+i = af_max + 1
 while i < (af_min + 100):
     f, dur, cycles, chords = mp[i]
     sub_path = '../audioGeneration/' + str(i)
     wav_path = sub_path + '.wav'
-    mp3_path = sub_path + '.mp3'
+    # mp3_path = sub_path + '.mp3'
     
     if os.path.exists(wav_path):
         os.system("rm " + wav_path)
-    if os.path.exists(mp3_path):
-        os.system("rm " + mp3_path)
+    # if os.path.exists(mp3_path):
+    #     os.system("rm " + mp3_path)
 
     os.system("python3 main.py " + str(f) + ' ' + str(dur) + ' ' + str(cycles) + ' ' + str(chords))
     os.system("sclang sc/nrt_all.scd " + str(dur + 30) + ' ' + str(i))
-    audio = AudioSegment.from_wav(wav_path)
-    audio.export(mp3_path, format='mp3')
+    # audio = AudioSegment.from_wav(wav_path)
+    # audio.export(mp3_path, format='mp3')
     
-    if os.path.exists(wav_path) and os.path.exists(mp3_path):
+    if os.path.exists(wav_path):
         i += 1
     
