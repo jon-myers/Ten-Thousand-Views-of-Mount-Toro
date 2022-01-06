306d13753d42d2cca2733f998aa37a78497445a5
Jon-myers <jon.myers.sound@gmail.com>
2022-01-06 12:11:09 -0800

    trying something else

--- test_process_running.py
+++ test_process_running.py
@@ -1,15 +1,7 @@
-import psutil
+import subprocess
 
-def check_if_process_running(process_name):
-    for proc in psutil.process_iter():
-        try:
-            if process_name.lower() in proc.name().lower():
-                return True
-        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
-            pass
-        return False;
-        
-if check_if_process_running('terminal_call.py'):
-    print('currently running')
-else:
-    print('not currently running')
+pytonProcess = subprocess.check_output("ps -ef | grep .py",shell=True).decode()
+pytonProcess = pytonProcess.split('\n')
+  
+for process in pytonProcess:
+    print(process)
