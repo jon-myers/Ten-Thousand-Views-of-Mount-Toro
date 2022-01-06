9f899e566981032081f697177d0b1d0bccc1ce10
Jon-myers <jon.myers.sound@gmail.com>
2022-01-06 12:06:02 -0800

    test process running

---
+++ test_process_running.py
@@ -0,0 +1,15 @@
+import psutil
+
+def check_if_process_running(process_name):
+    for proc in psutil.process_iter():
+        try:
+            if processName.lower() in proc.name().lower():
+                return True
+        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+            pass
+        return False;
+        
+if check_if_process_running('terminal_call.py'):
+    print('currently running')
+else:
+    print('not currently running')
