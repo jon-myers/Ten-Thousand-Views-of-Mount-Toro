79c41a93e20bae1c5b4f8d6bca013f9a9113c4a6
Jon-myers <jon.myers.sound@gmail.com>
2022-01-06 12:07:28 -0800

    bug fix

--- test_process_running.py
+++ test_process_running.py
@@ -3,7 +3,7 @@ import psutil
 def check_if_process_running(process_name):
     for proc in psutil.process_iter():
         try:
-            if processName.lower() in proc.name().lower():
+            if process_name.lower() in proc.name().lower():
                 return True
         except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
             pass
