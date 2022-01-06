9170c08301be5f0cf0dcc5f17c66ab55c8ce0a23
Jon-myers <jon.myers.sound@gmail.com>
2022-01-06 13:05:38 -0800

    inf_ct

--- inf_ct.py
+++ inf_ct.py
@@ -1,4 +1,14 @@
-a = True
-ct = 0
-while a:
-    ct += 1
+import os, sys
+
+pid = str(os.getpid())
+pidfile = 'mydaemon.pid'
+if os.path.isfile(pidfile):
+    print('%s already exists, exiting' % pidfile)
+    sys.exit()
+file(pidfile, 'w').write(pid)
+try:
+    ct = 0
+    while ct < 100000:
+        ct += 1
+finally:
+    os.unlink(pidfile)
