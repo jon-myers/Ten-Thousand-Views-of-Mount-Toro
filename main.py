3f13277959ca2c4882b6eb492bbda1a32d5c37b0
jon-myers <jon.myers.sound@gmail.com>
2022-02-25 19:09:43 -0800

    no. 82

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from piece import build
-import sys
+import sys, json
 # from line_profiler import LineProfiler
 # lprofiler = LineProfiler()
 # lp_wrapper = lprofiler(build)
@@ -7,16 +7,22 @@ import sys
 
 
 
-f = 0.6
-dur_tot = 24 * 60
-noc = 14
-num_of_modes = 16
+# f = 0.6
+# dur_tot = 24 * 60
+# noc = 14
+# num_of_modes = 16
+#
+# f = float(sys.argv[1])
+# dur_tot = float(sys.argv[2])
+# noc = int(float(sys.argv[3]))
+# num_of_modes = int(float(sys.argv[4]))
+# print(f, dur_tot, noc, num_of_modes)
 
-f = float(sys.argv[1])
-dur_tot = float(sys.argv[2])
-noc = int(float(sys.argv[3]))
-num_of_modes = int(float(sys.argv[4]))
-print(f, dur_tot, noc, num_of_modes)
+meta_params = json.load(open('JSON/meta_params.JSON', 'rb'))
+print(meta_params[81])
+f, dur_tot, noc, num_of_modes = meta_params[81]
+noc = int(noc)
+num_of_modes = int(num_of_modes)
 
-build(use_pickles=False, dur_tot=dur_tot, f=f, num_of_modes=num_of_modes, noc=noc)
+# build(True, False, True, dur_tot=dur_tot, f=f, num_of_modes=num_of_modes, noc=noc)
 # lp_wrapper(save_pickle=True)
